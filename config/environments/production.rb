9f6534ba15a565ddbac4c2d0970c884fcc5668a6
DevPomeis <pomeisisback@gmail.com>
2020-02-17 21:44:31 +0100

    devise fonctionnel

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "eventbrite-project_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'paceventbrite.herokuapp.com'}
-  Rails.application.routes.default_url_options[:host] = 'paceventbrite.herokuapp.com'
+  config.action_mailer.default_url_options = { host: 'https://paceventbrite.herokuapp.com'}
+  Rails.application.routes.default_url_options[:host] = 'https://paceventbrite.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
