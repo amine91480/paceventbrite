81aa636e09e0f748d517ae22b963cee49fd7eb9c
DevPomeis <pomeisisback@gmail.com>
2020-02-18 21:33:48 +0100

    fin mardi bonus + navbarlinks actif

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,33 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
-  before_action :check_user, only: [:show]
+  before_action :authenticate_user!
+  before_action :check_user
+  before_action :user
 
   def show
-    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   private 
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description)
+  end
   
   def check_user
     redirect_to current_user unless current_user.id.to_i == params[:id].to_i
+  end
+
+  def user
+    @user = User.find(params[:id])
   end
 end
