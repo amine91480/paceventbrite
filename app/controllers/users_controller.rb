61711791fa2936cbe40a0b973d799c8fcf1878f9
DevPomeis <pomeisisback@gmail.com>
2020-02-18 19:40:56 +0100

    Devise authenticate before certain actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  before_action :check_user, only: [:show]
+
   def show
+    @user = User.find(params[:id])
+  end
+
+  private 
+  
+  def check_user
+    redirect_to current_user unless current_user.id.to_i == params[:id].to_i
   end
 end
