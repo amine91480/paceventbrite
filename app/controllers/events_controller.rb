28a3f7e9d0566e96b2e4f9925ff22134ffc60a92
DevPomeis <pomeisisback@gmail.com>
2020-02-18 15:32:45 +0100

    Création d'évènements OK

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,8 +5,26 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.start_date = Event.convert_date_time(params[:date], params[:hour])
+    @event.admin = current_user
+    puts event_params
+    if @event.save
+      redirect_to @event
+    else 
+      @date = params[:date]
+      @hour = params[:hour]
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :location, :price, :duration)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -9,19 +9,35 @@ class Event < ApplicationRecord
   has_many :attendances
   has_many :users, through: :attendances
 
+  def self.convert_date_time(date, time)
+    (date.to_s + " " +time.to_s).to_datetime
+  end
+
   private 
   
   def cant_be_past
-    errors.add(:start_date, "cant be past") if start_date < Time.now
+    if start_date.nil?
+      errors.add(:start_date, "must be filled") 
+    else
+      errors.add(:start_date, "cant be past") if start_date < Time.now
+    end
   end
   
   def duration_5_pos
-    errors.add(:duration, "has to be a multiple of 5") unless duration % 5 == 0
-    errors.add(:duration, "has to be a positive amount of minutes") unless duration > 0
+    if duration.nil?
+      errors.add(:duration, "must be filled") 
+    else
+      errors.add(:duration, "has to be a multiple of 5") unless duration % 5 == 0
+      errors.add(:duration, "has to be a positive amount of minutes") unless duration > 0
+    end
   end
 
   def price_validation
-    errors.add(:price, "cant be free") if price < 1
-    errors.add(:price, "cant be over 1000") if price > 1000
+    if price.nil?
+      errors.add(:price, "must be filled") 
+    else
+      errors.add(:price, "cant be free") if price < 1
+      errors.add(:price, "cant be over 1000") if price > 1000
+    end
   end
 end
